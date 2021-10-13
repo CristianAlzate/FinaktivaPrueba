c9f6361cae36003ece14ec481677dca5c7d3c474
Cristian1119 <alzatecristian@gmail.com>
2021-10-13 13:52:42 -0500

    Inicializacion del repositorio y creacion de estructura del proyecto

---
+++ Finaktiva.Repository/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Finaktiva.Repository
+{
+    public class Context : DbContext
+    {
+
+    }
+}
